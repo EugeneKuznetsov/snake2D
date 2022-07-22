6a0dd7e21875556e734f7e7608806383d50cc694
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-07-22 22:05:34 +0200

    tests: snake movements use cases
    related to #8

--- src/game/Snake.cpp
+++ src/game/Snake.cpp
@@ -1,8 +1,18 @@
 #include "Snake2D/Snake.hpp"
 
+#include "Snake2D/Playfield.hpp"
+
 Snake::Snake(const Position& position)
     : position_{position}
+    , direction_{Direction::invalid}
 {
     position_.emplace_back(Position{position.row, static_cast<unsigned short>(position.col + 1u)});
     position_.emplace_back(Position{position.row, static_cast<unsigned short>(position.col + 2u)});
 }
+
+auto Snake::direction(const Direction& direction) -> void
+{
+    direction_ = direction;
+}
+
+auto Snake::move(const Playfield& /*playfield*/) -> void {}
--- tests/test_Snake.cpp
+++ tests/test_Snake.cpp
@@ -18,12 +18,59 @@ public:
 
 public:
     std::unique_ptr<Snake> snake_{nullptr};
-    const Position head_position_{1u, 1u};
-    const Position torso_position_{head_position_.row, static_cast<unsigned short>(head_position_.col + 1u)};
-    const Position tail_position_{torso_position_.row, static_cast<unsigned short>(torso_position_.col + 1u)};
+    Position head_position_{1u, 1u};
+    Position torso_position_{head_position_.row, static_cast<unsigned short>(head_position_.col + 1u)};
+    Position tail_position_{torso_position_.row, static_cast<unsigned short>(torso_position_.col + 1u)};
+    const Playfield playfield_{4u, 4u};
 };
 
 TEST_F(the_snake, initializes_horizontally_positions_for_head_torso_and_tail)
 {
+    EXPECT_POSITION({head_position_, torso_position_, tail_position_});
+}
+
+TEST_F(the_snake, does_not_move_without_setting_direction_for_the_first_time)
+{
+    snake_->move(playfield_);
+    EXPECT_POSITION({head_position_, torso_position_, tail_position_});
+}
+
+TEST_F(the_snake, head_moves_to_opposite_side_of_playfield_horizontally)
+{
+    snake_->direction(Direction::left);
+    snake_->move(playfield_);
+    head_position_.col = playfield_.cols;
+    torso_position_.col--;
+    tail_position_.col--;
+    EXPECT_POSITION({head_position_, torso_position_, tail_position_});
+}
+
+TEST_F(the_snake, head_moves_to_opposite_side_of_playfield_vertically)
+{
+    snake_->direction(Direction::up);
+    snake_->move(playfield_);
+    head_position_.row = playfield_.rows;
+    torso_position_.col--;
+    tail_position_.col--;
+    EXPECT_POSITION({head_position_, torso_position_, tail_position_});
+}
+
+TEST_F(the_snake, head_moves_down_when_changing_direction_down)
+{
+    snake_->direction(Direction::down);
+    snake_->move(playfield_);
+    head_position_.row++;
+    torso_position_.col--;
+    tail_position_.col--;
+    EXPECT_POSITION({head_position_, torso_position_, tail_position_});
+}
+
+TEST_F(the_snake, head_will_not_move_towards_torso_when_changing_direction_towards_torso)
+{
+    snake_->direction(Direction::right);
+    snake_->move(playfield_);
+    head_position_.col = playfield_.cols;
+    torso_position_.col--;
+    tail_position_.col--;
     EXPECT_POSITION({head_position_, torso_position_, tail_position_});
 }
